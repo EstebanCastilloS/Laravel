963aec995c9649f4648b473f6e70bd65e9a5d70e
EstebanCastillo <ncastillo308@unab.edu.co>
2023-11-17 23:47:00 -0500

    Agregando cambios a la platilla y modificando los campos

--- 09_Control_Asistencia/routes/web.php
+++ 09_Control_Asistencia/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -17,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->middleware('auth')->name('/');
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
