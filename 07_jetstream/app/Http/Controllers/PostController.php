4f98a9ff5fef75b6d79afa8138401d0d867579a1
EstebanCastillo <ncastillo308@unab.edu.co>
2023-11-21 20:24:09 -0500

    Formas de Seguridad

--- 07_jetstream/app/Http/Controllers/PostController.php
+++ 07_jetstream/app/Http/Controllers/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::latest('id')->paginate(10);
+        $posts = Post::where('user_id',auth()->id())->latest('id')->paginate(10);
         return view('admin.posts.index', compact('posts'));
     }
 
