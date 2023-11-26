0c4db1b43c3890de2147d008ba9aed743bc323a4
EstebanCastillo <ncastillo308@unab.edu.co>
2023-11-26 14:35:09 -0500

    Finalizando las imagenes en el show

--- 09_Control_Asistencia/resources/views/miembros/show.blade.php
+++ 09_Control_Asistencia/resources/views/miembros/show.blade.php
@@ -37,7 +37,8 @@
                                     <div class="col-md-6">
                                         <div class="fotm-group">
                                             <label for="">Email*</label>
-                                            <input type="email" name="email" class="form-control" value ="{{ $member->email }}" disabled>
+                                            <input type="email" name="email" class="form-control"
+                                                value ="{{ $member->email }}" disabled>
                                             @error('email')
                                                 <small style="color: red">El campo Email es Requerido</small>
                                             @enderror
@@ -48,7 +49,8 @@
                                     <div class="col-md-6">
                                         <div class="fotm-group">
                                             <label for="">Telefono*</label>
-                                            <input type="text" name="phone" class="form-control" value ="{{ $member->phone }}" disabled>
+                                            <input type="text" name="phone" class="form-control"
+                                                value ="{{ $member->phone }}" disabled>
                                             @error('phone')
                                                 <small style="color: red">El campo Telefono es Requerido</small>
                                             @enderror
@@ -59,7 +61,8 @@
                                     <div class="col-md-6">
                                         <div class="fotm-group">
                                             <label for="">Dirección*</label>
-                                            <input type="text" name="address" class="form-control" value ="{{ $member->address }}"  disabled>
+                                            <input type="text" name="address" class="form-control"
+                                                value ="{{ $member->address }}" disabled>
                                             @error('address')
                                                 <small style="color: red">El campo Dirección es Requerido</small>
                                             @enderror
@@ -70,7 +73,8 @@
                                     <div class="col-md-6">
                                         <div class="fotm-group">
                                             <label for="">Fecha de Nacimiento</label>
-                                            <input type="date" name="birthdate" class="form-control" value ="{{ $member->birthdate }}" disabled>
+                                            <input type="date" name="birthdate" class="form-control"
+                                                value ="{{ $member->birthdate }}" disabled>
                                             @error('birthdate')
                                                 <small style="color: red">El campo Fecha de Nacimiento es Requerido</small>
                                             @enderror
@@ -83,8 +87,11 @@
                                             <label for="gender">Género</label>
                                             <select id="gender" name="gender" class="form-control" disabled>
                                                 <option value="">Selecciona un género</option>
-                                                <option value="Masculino" {{ $member->gender == 'Masculino' ? 'selected' : '' }}>Masculino</option>
-                                                <option value="Femenino" {{ $member->gender == 'Femenino' ? 'selected' : '' }}>Femenino</option>
+                                                <option value="Masculino"
+                                                    {{ $member->gender == 'Masculino' ? 'selected' : '' }}>Masculino
+                                                </option>
+                                                <option value="Femenino"
+                                                    {{ $member->gender == 'Femenino' ? 'selected' : '' }}>Femenino</option>
                                                 <option value="Otro">Otro</option>
                                                 <!-- Añade más opciones aquí si es necesario -->
                                             </select>
@@ -100,8 +107,11 @@
                                             <label for="status">Estado</label>
                                             <select id="status" name="status" class="form-control" disabled>
                                                 <option value="">Selecciona un estado</option>
-                                                <option value="active"{{ $member->status == 'active' ? 'selected' : '' }}>Activo</option>
-                                                <option value="inactive"{{ $member->status == 'inactive' ? 'selected' : '' }}>Inactivo</option>
+                                                <option value="active"{{ $member->status == 'active' ? 'selected' : '' }}>
+                                                    Activo</option>
+                                                <option
+                                                    value="inactive"{{ $member->status == 'inactive' ? 'selected' : '' }}>
+                                                    Inactivo</option>
                                                 <!-- Añade más opciones aquí si es necesario -->
                                             </select>
                                             @error('status')
@@ -116,14 +126,28 @@
                                             <label for="ministry">Ministerio</label>
                                             <select id="ministry" name="ministry" class="form-control" disabled>
                                                 <option value="">Selecciona un ministerio</option>
-                                                <option value="Ancianos"{{ $member->ministry == 'Ancianos' ? 'selected' : '' }}>Ancianos</option>
-                                                <option value="Adultos"{{ $member->ministry == 'Adultos' ? 'selected' : '' }}>Adultos</option>
-                                                <option value="Jovenes"{{ $member->ministry == 'Jovenes' ? 'selected' : '' }}>Jovenes</option>
-                                                <option value="Adolescentes"{{ $member->ministry == 'Adolescentes' ? 'selected' : '' }}>Adolescentes</option>
-                                                <option value="Niños"{{ $member->ministry == 'Niños' ? 'selected' : '' }}>Niños</option>
-                                                <option value="Intercesion"{{ $member->ministry == 'Intercesion' ? 'selected' : '' }}>Intercesion</option>
-                                                <option value="Alabanza"{{ $member->ministry == 'Alabanza' ? 'selected' : '' }}>Alabanza</option>
-                                                <option value="Danza"{{ $member->ministry == 'Danza' ? 'selected' : '' }}>Danza</option>
+                                                <option
+                                                    value="Ancianos"{{ $member->ministry == 'Ancianos' ? 'selected' : '' }}>
+                                                    Ancianos</option>
+                                                <option
+                                                    value="Adultos"{{ $member->ministry == 'Adultos' ? 'selected' : '' }}>
+                                                    Adultos</option>
+                                                <option
+                                                    value="Jovenes"{{ $member->ministry == 'Jovenes' ? 'selected' : '' }}>
+                                                    Jovenes</option>
+                                                <option
+                                                    value="Adolescentes"{{ $member->ministry == 'Adolescentes' ? 'selected' : '' }}>
+                                                    Adolescentes</option>
+                                                <option value="Niños"{{ $member->ministry == 'Niños' ? 'selected' : '' }}>
+                                                    Niños</option>
+                                                <option
+                                                    value="Intercesion"{{ $member->ministry == 'Intercesion' ? 'selected' : '' }}>
+                                                    Intercesion</option>
+                                                <option
+                                                    value="Alabanza"{{ $member->ministry == 'Alabanza' ? 'selected' : '' }}>
+                                                    Alabanza</option>
+                                                <option value="Danza"{{ $member->ministry == 'Danza' ? 'selected' : '' }}>
+                                                    Danza</option>
                                             </select>
                                             @error('ministry')
                                                 <small style="color: red">El campo Ministerio es Requerido</small>
@@ -135,7 +159,8 @@
                                     <div class="col-md-6">
                                         <div class="fotm-group">
                                             <label for="">Fecha de Ingreso</label>
-                                            <input type="date" name="date_admission" class="form-control" value ="{{ $member->date_admission }}" disabled>
+                                            <input type="date" name="date_admission" class="form-control"
+                                                value ="{{ $member->date_admission }}" disabled>
                                             @error('date_admission')
                                                 <small style="color: red">El campo Fecha de Ingreso es Requerido</small>
                                             @enderror
@@ -144,32 +169,34 @@
 
                                     {{-- Fotografía --}}
                                     <div class="col-md-6">
-                                        <div class="col-md-6">
-                                            <div class="fotm-group">
-                                                <label for="">Fotografía</label>
-                                                <input type="file" name="photo" class="form-control" id="file-input"
-                                                    onchange="archivo(event)">
-                                                <center><output id="list"></output></center>
-                                                @error('photo')
-                                                    <small style="color: red">El campo Fotografía es Requerido</small>
-                                                @enderror
-                                            </div>
+                                        <div class="fotm-group">
+                                            <label for="">Fotografía</label>
+                                            <br>
+
+                                            @if ($member->photo == '')
+
+                                                @if ($member->gender == 'Masculino')
+                                                    <img src="{{ asset('images/avatar_hombre_1.jpg') }}" width="150px" alt="">
+
+                                                @else
+                                                    <img src="{{ asset('images/avatar_mujer.png') }}" width="150px" alt="">
+
+                                                @endif
+
+                                            @else
+                                                <center class="pd-1">
+                                                    <img src="{{ asset('storage') . '/' . $member->photo }}" alt=""
+                                                        id="imgSalida" width="200px" height="200px" >
+                                                </center>
+
+                                            @endif
+
                                         </div>
                                     </div>
 
                                 </div>
                             </div>
                         </div>
-
-                        <br />
-                        <div class="row">
-                            <div class="col-md-12">
-                                <button class="btn btn-primary " style="margin-right: 10px"
-                                    type="submit">Guardar</button>
-                                <a href="" class="btn btn-danger ">Cancelar</a>
-                            </div>
-                        </div>
-
                     </div>
 
                 </div>
