4362a0dbffc181560084d03e7cdf82bb407fd7cd
EstebanCastillo <ncastillo308@unab.edu.co>
2024-01-07 15:37:41 -0500

    Creación de Product -a y trabajando Migración

---
+++ 10_Sistema_Ventas/database/migrations/2024_01_07_202740_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->text('description')->nullable(); //descripcion
+            $table->unsignedInteger('purchase_price')->nullable(); //precio_compra
+            $table->unsignedInteger('sales_price')->nullable(); //precio_venta
+            $table->unsignedInteger('minimum_price')->nullable(); //precio_minimo
+            $table->integer('stock')->nullable()->default(0); //stock
+            $table->integer('minimum_stock')->nullable()->default(10); //stock_minimo
+            $table->string('image')->nullable(); //imagen
+            $table->string('code')->nullable(); //codigo
+            $table->string('code_bars')->nullable(); //codigo_barras
+            $table->date('expiration_date')->nullable(); //fecha_vencimiento
+            $table->boolean('active')->default(true); //activo
+            $table->tinyInteger('iva')->default(0); //iva
+
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
