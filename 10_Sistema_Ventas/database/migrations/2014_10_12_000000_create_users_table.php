45ac118912ad6d0dba80b25b46fb088710a0fb53
EstebanCastillo <ncastillo308@unab.edu.co>
2024-01-07 15:52:40 -0500

    Agregando campos a la Migración de usuario

--- 10_Sistema_Ventas/database/migrations/2014_10_12_000000_create_users_table.php
+++ 10_Sistema_Ventas/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('admin')->default(true); // admin
+            $table->boolean('active')->default(true); // active
             $table->rememberToken();
             $table->timestamps();
         });
