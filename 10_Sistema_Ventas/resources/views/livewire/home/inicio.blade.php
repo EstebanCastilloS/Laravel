4f765f817850ffd4a2b207ff60a013723ed13325
EstebanCastillo <ncastillo308@unab.edu.co>
2024-01-09 15:34:02 -0500

    Creando el componenete Card

--- 10_Sistema_Ventas/resources/views/livewire/home/inicio.blade.php
+++ 10_Sistema_Ventas/resources/views/livewire/home/inicio.blade.php
@@ -1,3 +1,10 @@
 <div>
     <h1>Componente de Inicio</h1>
+
+    <x-card cardTitle="card Title" cardFooter="card Footer">
+        <x-slot:cardTools>
+            <a href="" class="btn btn-primary">Crear</a>
+        </x-slot:cardTools>
+        Contenido del componente
+    </x-card>
 </div>
