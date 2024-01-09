8a7f7b48a1c6de28dcdee1dfd6091d488e683ff1
EstebanCastillo <ncastillo308@unab.edu.co>
2024-01-09 14:13:27 -0500

    Mostrando plantilla inicio con adminLite

--- 10_Sistema_Ventas/routes/web.php
+++ 10_Sistema_Ventas/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Home\Inicio;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +23,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/inicio', Inicio::class)->name('inicio');
