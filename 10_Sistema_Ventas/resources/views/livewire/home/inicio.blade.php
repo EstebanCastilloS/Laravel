7b038ccc2d493070ef6a27ed95b5138caa65c0d6
EstebanCastillo <ncastillo308@unab.edu.co>
2024-01-09 16:01:13 -0500

    Creando el componenete table

--- 10_Sistema_Ventas/resources/views/livewire/home/inicio.blade.php
+++ 10_Sistema_Ventas/resources/views/livewire/home/inicio.blade.php
@@ -5,6 +5,26 @@
         <x-slot:cardTools>
             <a href="" class="btn btn-primary">Crear</a>
         </x-slot:cardTools>
-        Contenido del componente
+
+        <x-table>
+            <x-slot:thead>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Descripción</th>
+                <th>Acciones</th>
+            </x-slot>
+
+            <tr>
+                <td>1</td>
+                <td>Esteban</td>
+                <td>Castillo</td>
+                <td>
+                    <a href="" class="btn btn-primary">Editar</a>
+                    <a href="" class="btn btn-danger">Eliminar</a>
+                </td>
+            </tr>
+
+        </x-table>
+
     </x-card>
 </div>
