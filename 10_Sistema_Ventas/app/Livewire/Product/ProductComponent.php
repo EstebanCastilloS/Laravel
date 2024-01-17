97e0ad23d0921ed4241906f78bed6a11b4a648f1
EstebanCastillo <ncastillo308@unab.edu.co>
2024-01-17 09:02:29 -0500

    Product: Validaciones en el formulario

--- 10_Sistema_Ventas/app/Livewire/Product/ProductComponent.php
+++ 10_Sistema_Ventas/app/Livewire/Product/ProductComponent.php
@@ -22,7 +22,17 @@ class ProductComponent extends Component
 
     //propiedad de modelo
     public $name;
-    public $id;
+    public $id=0;
+    public $category_id;
+    public $description;
+    public $purchase_price;
+    public $sales_price;
+    public $code_bars;
+    public $stock=10;
+    public $minimum_stock;
+    public $expiration_date;
+    public $active=1;
+    public $image;
 
     public function render()
     {
@@ -48,5 +58,36 @@ class ProductComponent extends Component
 
         //abrir modal
         $this->dispatch('open-modal','modalProduct');
+    }
+
+    public function store(){
+        //dump('Crear producto');
+
+        $rules = [
+            'name' => 'required|unique:products,name,'.$this->id,
+            'description' => 'max:255',
+            'purchase_price' => 'numeric|nullable',
+            'sales_price' => 'required|numeric',
+            'stock' => 'required|numeric',
+            'minimum_stock' => 'numeric|nullable',
+            'image' => 'image|max:1024|nullable',
+            'category_id' => 'required|numeric',
+            'code_bars' => 'required',
+            'expiration_date' => 'required',
+            'active' => 'required',
+
+        ];
+
+
+        $this->validate($rules);
+
+        // $category = new Category();
+        // $category->name = $this->name;
+        // $category->save();
+
+        // $this->dispatch('close-modal','modalCategory');
+        // $this->dispatch('msg','Categoria creada correctamente.');
+
+        // $this->reset(['name']);
     }
 }
